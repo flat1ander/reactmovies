702334b42fd30bd69bfb4a4a7644ec3c81875c7c
flat1ander <thematthewlane@gmail.com>
2023-05-01 19:30:53 -0400

    initial setup of Form & MovieDisplay components

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+
+const Form = (props) => {
+    return <h1>The Form Component</h1>
+};
+
+
+
+
+
+
+
+export default Form;
---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,18 @@
+
+const MovieDisplay = (props) => {
+    return <h1>The MovieDisplay Component</h1>
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+export default MovieDisplay;
