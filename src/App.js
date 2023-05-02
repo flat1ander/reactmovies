08b27ed6e24d0e3617560b3a5d0a4b157a3edada
flat1ander <thematthewlane@gmail.com>
2023-05-01 21:30:27 -0400

    Almost there

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
 
 function App() {
+  const [movie, setMovie] = useState(null);
+  const apiKey = '3abc7fd5';
+
+  // function to fetch movie data
+  //searchTerm is form data from Form.js
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    )
+    // parse the returned JSON
+    const data = await response.json();
+  }
+
+ 
+  //call getMovie fucntion
+  getMovie('dune')
+
+  // Everything in this return is JSX (which is JS and XML):
   return (
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      {/* pass the getMovie function to MovieDisplay */}
+      <Form movieSearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,41 @@
-
+import { useState } from "react";
 const Form = (props) => {
-    return <h1>The Form Component</h1>
+    //instantiate a new instance of state
+    const [formData, setFormData] = useState({searchTerm: ''})
+    
+    // handleChange function to capture/update formData
+    const handleChange = (event) => {
+        // console.log('event', event.target.name, event.target.value)
+        //when you set state you create a brand new object. In this case it's a new ojbect with all the previous key:values and the key we want to update
+        setFormData({
+            //[event.target.name] is a way to target an object dynamically
+            // you always spread the previous object first... then update specific key:values
+            ...formData,[event.target.name]: event.target.value
+        })
+    }
+
+    //handleSubmit function will submit lift state up to App.js
+    const handleSubmit = (event) => {
+        // the natural behavior of a form is to refresh the page which we don't want to do:
+        event.preventDefault();
+        // call the movieSearch fucntion from App.js and pass it our form data:
+        props.movieSearch(formData.searchTerm)
+    }
+
+    return (
+        <div>
+           <form onSubmit = {handleSubmit}>
+            <input 
+                type="text" 
+                name='searchTerm'
+                value={formData.searchTerm}
+                onChange={handleChange}
+            />
+            <input type="submit" value ="submit" />
+            </form>     
+        </div>
+
+    )
 };
 
 
